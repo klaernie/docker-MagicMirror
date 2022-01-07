8f535062ebef1885948377b7354a282dcc617567
Andre Klärner <kandre@ak-online.be>
2022-01-07 23:13:24 +0100

    reference only my namespace and use ghcr.io

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 # https://stackoverflow.com/a/51761312/4934537
 latest_release=$(git ls-remote --tags --refs --sort="v:refname" https://github.com/MichMich/MagicMirror.git | tail -n1 | sed 's/.*\///')
-if [ "$(DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect bastilimbach/docker-magicmirror:"${latest_release}" > /dev/null; echo $?)" != 0 ]; then
-  docker buildx build --progress plain --platform=linux/amd64,linux/arm64,linux/arm/v7 "${1}" --build-arg branch="${latest_release}" -t bastilimbach/docker-magicmirror:"${latest_release}" -t bastilimbach/docker-magicmirror:latest .
+if [ "$(DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect ghcr.io/klaernie/docker-magicmirror:"${latest_release}" > /dev/null; echo $?)" != 0 ]; then
+  docker buildx build --progress plain --platform=linux/amd64,linux/arm64,linux/arm/v7 "${1}" --build-arg branch="${latest_release}" -t ghcr.io/klaernie/docker-magicmirror:"${latest_release}" -t ghcr.io/klaernie/docker-magicmirror:latest .
 fi
 
-docker buildx build --progress plain --platform=linux/amd64,linux/arm64,linux/arm/v7 "${1}" --build-arg branch=develop -t bastilimbach/docker-magicmirror:develop .
+docker buildx build --progress plain --platform=linux/amd64,linux/arm64,linux/arm/v7 "${1}" --build-arg branch=develop -t ghcr.io/klaernie/docker-magicmirror:develop .
