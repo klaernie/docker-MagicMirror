7041eedebb03e2c055e0f71582c5a5478346fb5c
Andre Klärner <kandre@ak-online.be>
2023-12-26 00:14:54 +0100

    continually overwrite the mm-versioned tag in the container registry, to refresh existing versions with newer packaging

--- build.sh
+++ build.sh
@@ -7,8 +7,5 @@ push=""
 
 # https://stackoverflow.com/a/51761312/4934537
 latest_release=$(git ls-remote --tags --refs --sort="v:refname" https://github.com/MichMich/MagicMirror.git | tail -n1 | sed 's/.*\///')
-if [ "$(DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect ghcr.io/klaernie/docker-magicmirror:"${latest_release}" > /dev/null; echo $?)" != 0 ]; then
-  docker buildx build --progress plain --platform=linux/amd64,linux/arm64,linux/arm/v7 ${push:+"--push"} --build-arg branch="${latest_release}" -t ghcr.io/klaernie/docker-magicmirror:"${latest_release}" -t ghcr.io/klaernie/docker-magicmirror:latest .
-fi
 
-docker buildx build --progress plain --platform=linux/amd64,linux/arm64,linux/arm/v7 ${push:+"--push"} --build-arg branch=develop -t ghcr.io/klaernie/docker-magicmirror:develop .
+docker buildx build --progress plain --platform=linux/amd64,linux/arm64,linux/arm/v7 ${push:+"--push"} --build-arg branch="${latest_release}" -t ghcr.io/klaernie/docker-magicmirror:"${latest_release}" -t ghcr.io/klaernie/docker-magicmirror:latest .
